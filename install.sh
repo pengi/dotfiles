aaa90ce7e54e3bbead5aadf474e1052f420ef8c0
Max Sikström <max@pengi.se>
2026-03-02 06:15:26 +0000

    Add printout to install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,11 @@
+echo "======================================================================="
+echo "=== Updateing dotilfes"
+echo "==="
+echo "=== $0"
+echo "=== PWD  = $PWD"
+echo "=== HOME = $HOME"
+echo "==="
+echo "======================================================================="
+
 git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh
 git -C $HOME/.oh-my-zsh checkout master
